ad122588c418611648f13396076cb37509d8fa68
Daniel Britt <28036018+djbritt@users.noreply.github.com>
2024-04-12 23:28:41 -0700

    Update building-bufferGeometry-conversion.js

--- Procedural City_files/building-bufferGeometry-conversion.js
+++ Procedural City_files/building-bufferGeometry-conversion.js
@@ -10,89 +10,7 @@ function Building(xPos, yPos) {
     self.object.position.x = self.x;
     self.object.position.y = self.y;
     self.object.position.z = -0.1;
-    // self.object.position.z = (noise.simplex2(xPos * 0.0005, yPos * 0.0005) + 1) / 2 * -80;
-    // self.object.position.z /= (noise.simplex2(xPos * 0.0001, yPos * 0.0001) + 1) / 2 * 1;
-
-    /// for testing purposes
-    // var cubeGeo = new THREE.BoxGeometry(1, 1, 1);
-    // var cubeMat = new THREE.MeshPhongMaterial({color: 0x00ffff});
-    // var cube = new THREE.Mesh(cubeGeo, cubeMat);
-    // self.object.add(cube);
-
-		// self.setFaceColor = function (geometry, color) {
-			//console.log("INSIDE SETVERTEXT COLOR----------------")
-			//console.log(geometry)
-			//console.log("-------------------")
-		// 		var colors = [];
-		// 		// for (var i = 0; i < geometry.attributes.position.count; i++) {
-		// 		// 		colors.push(color.r, color.g, color.b);
-		// 		// }
-		// 		for ( var i = 0; i < geometry.faces.length; i ++ ) {
-		// 				var f  = geometry.faces[ i ];
-		// 				for( var k = 0; k < 3; k++ )
-		// 				{
-		// 					f.vertexColors[ k ] = color;
-		// 				}
-		// 		}
-		// 		// geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
-		// }
-
-		// self.setFaceColor = function(geo, color) {
-    //
-		// 	console.log("INSIDE setFaceColor COLOR----------------")
-		// 	console.log(geo)
-		// 	console.log("COLOR IS")
-		// 	console.log(color)
-		// 	console.log("-------------------")
-		// 	// Access the vertex positions attribute
-		// 	const positions = geo.attributes.position.array;
-    //
-		// 	// Assuming each face is a triangle (3 vertices)
-		// 	for (let i = 0; i < positions.length; i += 3) {
-		// 		// Set the color for all three vertices of a triangle
-		// 		positions[i] = color.r; // Set red component
-		// 		positions[i + 1] = color.g; // Set green component
-		// 		positions[i + 2] = color.b; // Set blue component
-		// 	}
-    //
-		// 	// Update the vertex data attribute (important for rendering)
-		// 	geo.attributes.position.needsUpdate = true;
-		// }
-
-    // self.setFaceColor = function(geo, color) {
-    //   console.log("INSIDE setFaceColor COLOR----------------");
-    //   console.log(geo);
-    //   console.log("COLOR IS");
-    //   console.log(color);
-    //   console.log("-------------------");
-    //
-    //   // Access the vertex colors attribute
-    //   if (!geo.attributes.color) {
-    //      const colors = new Float32Array(geo.attributes.position.count * 3); // three components per vertex
-    //      geo.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-    //  }
-    //
-    //   const colorAttribute = geo.attributes.color;
-    //
-    //   // Assuming each face is a triangle (3 vertices)
-    //   for (let i = 0; i < colorAttribute.array.length; i += 9) {
-    //     // Set the color for all three vertices of a triangle
-    //     colorAttribute.array[i] = color.r; // Set red component
-    //     colorAttribute.array[i + 1] = color.g; // Set green component
-    //     colorAttribute.array[i + 2] = color.b; // Set blue component
-    //
-    //     colorAttribute.array[i + 3] = color.r;
-    //     colorAttribute.array[i + 4] = color.g;
-    //     colorAttribute.array[i + 5] = color.b;
-    //
-    //     colorAttribute.array[i + 6] = color.r;
-    //     colorAttribute.array[i + 7] = color.g;
-    //     colorAttribute.array[i + 8] = color.b;
-    //   }
-    //
-    //   // Update the vertex color data attribute (important for rendering)
-    //   colorAttribute.needsUpdate = true;
-    // };
+    
 
     self.setFaceColor = function(geo, color) {
         console.log("INSIDE setFaceColor COLOR----------------");
@@ -130,18 +48,11 @@ function Building(xPos, yPos) {
     var saturation = Math.random() * 0.5 + 0.5;
     // saturation = 1;
 
-    // self.geometry = new THREE.BoxGeometry(100, 100, 1);
-    // self.geometry = new THREE.CylinderGeometry(80, 80, 1, 8);
-    self.geometry = new THREE.BoxGeometry(80, 80, 1, 8);
+    self.geometry = new THREE.BoxGeometry();
     self.geometry.rotateX(Math.PI / 2);
     // self.geometry.rotateZ(Math.PI/4);
     self.geometry.rotateZ(Math.random(Math.PI));
-    // self.geometry.translate(0, 0, 150);
-    //console.log("LINE 55 sending self.bufferGeometry-------------");
-    //console.log(self.geometry);
-    //console.log("COLOR VALUE!!!!");
-    //console.log(new THREE.Color().setHSL(hue, 0.0, 0.1));
-    //console.log("------------------------");
+   
     self.setFaceColor(self.geometry, new THREE.Color().setHSL(0.0, 0.0, 0.1));
     self.isDead = false;
 
@@ -173,40 +84,7 @@ function Building(xPos, yPos) {
 
     var symmetryType = Math.floor(Math.random() * 2.5);
 
-		// self.shiftLeftEdge = function(geo, amount) {
-		// 	//console.log("INSIDE SHIFT LEFT EDGE!!!!!!-------------")
-		//
-		// 	//console.log(geo)
-		// 	//console.log("-----------------------------------------")
-		//     geo.attributes.position.array[6 * 3] += amount;
-		//     geo.attributes.position.array[4 * 3] += amount;
-		//     geo.attributes.position.needsUpdate = true;
-		// }
-		//
-		// self.shiftRightEdge = function(geo, amount) {
-		//     geo.attributes.position.array[1 * 3] += amount;
-		//     geo.attributes.position.array[3 * 3] += amount;
-		//     geo.attributes.position.needsUpdate = true;
-		// }
-		//
-		// self.shiftNearEdge = function(geo, amount) {
-		//     geo.attributes.position.array[1 * 3 + 1] += amount;
-		//     geo.attributes.position.array[4 * 3 + 1] += amount;
-		//     geo.attributes.position.needsUpdate = true;
-		// }
-		//
-		// self.shiftFarEdge = function(geo, amount) {
-		//     geo.attributes.position.array[6 * 3 + 1] += amount;
-		//     geo.attributes.position.array[3 * 3 + 1] += amount;
-		//     geo.attributes.position.needsUpdate = true;
-		// }
-		//
-		// self.slopeEdge = function(geo, edge, amount) {
-		//     var edges = [[6, 4], [1, 4], [1, 3], [6, 3]];
-		//     geo.attributes.position.array[edges[edge][0] * 3 + 2] += amount;
-		//     geo.attributes.position.array[edges[edge][1] * 3 + 2] += amount;
-		//     geo.attributes.position.needsUpdate = true;
-		// }
+		
 
 		self.shiftLeftEdge = function(geo, amount) {
 		    // Accessing the position attribute's array directly
@@ -256,35 +134,7 @@ function Building(xPos, yPos) {
 		};
 
 
-		// self.shiftLeftEdge = function(geo, amount) {
-		// 	//console.log("INSIDE SHIFTLEFTEDGE--------")
-		// 	//console.log(geo)
-		// 	//console.log("------------------------------")
-		// 	geo.vertices[6].x += amount;
-		// 	geo.vertices[4].x += amount;
-		// }
-		//
-		// self.shiftRightEdge = function(geo, amount) {
-		//
-		// 	geo.vertices[1].x += amount;
-		// 	geo.vertices[3].x += amount;
-		// }
-		//
-		// self.shiftNearEdge = function(geo, amount) {
-		// 	geo.vertices[1].y += amount;
-		// 	geo.vertices[4].y += amount;
-		// }
-		//
-		// self.shiftFarEdge = function(geo, amount) {
-		// 	geo.vertices[6].y += amount;
-		// 	geo.vertices[3].y += amount;
-		// }
-		//
-		// self.slopeEdge = function(geo, edge, amount) {
-		// 	var edges = [[6, 4], [1, 4], [1, 3], [6, 3]];
-		// 	geo.vertices[edges[edge][0]].z += amount;
-		// 	geo.vertices[edges[edge][1]].z += amount;
-		// }
+		
 
     self.createPart = function () {
         var width = Math.random() * (maxWidth - minWidth) + minWidth;
@@ -305,23 +155,8 @@ function Building(xPos, yPos) {
         var saturation = Math.random() * 0.5 + 0.5;
         var color = new THREE.Color().setHSL(hue + Math.random() * 0.0, saturation, lightness);
 
-				var partGeo = new THREE.BoxGeometry(width, depth, height);
-				//console.log("PARTGEO IS LINE 149")
-				//console.log(partGeo)
-				//console.log("----------------------")
-
-				// const geometries = [partGeo];
-
-				// const mergedGeometry = THREE.BufferGeometryUtils.mergeGeometries(geometries);
-				//console.log("MERGED GEOMETRY")
-				//console.log(mergedGeometry)
-
-				// partGeo = mergedGeometry;
-				// partGeo = THREE.BufferGeometry().fromGeometry(partGeo)
-
-				//console.log("DOES CONVERSION WORK??? LINE 201----------")
-				//console.log(partGeo)
-				//console.log("-----------------------")
+	var partGeo = new THREE.BoxGeometry(width, depth, height);
+				
 
         /// shift top edges in a random amount
         var rightShift = -Math.random() * width / 2 * slopeIntensity;
@@ -342,58 +177,26 @@ function Building(xPos, yPos) {
 
         self.slopeEdge(partGeo, slopeEdge, slopeAmount);
 
-        // Create a color buffer attribute
-        // var colors = [];
-				//console.log("AT Create a color buffer attribute!!!-------")
-				//console.log(partGeo)
-				//console.log("---------------------------")
-        // for (var i = 0; i < partGeo.attributes.position.count; i++) {
-        //     colors.push(color.r, color.g, color.b);
-        // }
-        // partGeo.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
 
         var rot = Math.random(Math.PI * 0.001);
 
-        // if (isSymmetric) partGeo.rotateZ(rot);
-        // partGeo.applyMatrix4(new THREE.Matrix4().makeTranslation(X, Y, Z));
-        //console.log("PARTGEO IS");
-        //console.log(partGeo);
-        // self.geometry.merge(partGeo);
-        // partGeo.dispose();
+      
         self.setFaceColor(partGeo, color);
 
         partGeo.translate(X, Y, Z);
 
-				//console.log("PARTGEO IS")
-				//console.log(partGeo)
-				// self.geometry.merge(partGeo);
-
-
         if (symmetryType > 0) {
             var partGeo2 = new THREE.BoxGeometry();
             partGeo2.copy(partGeo);
-						//console.log("PARTGEO2 ISS!!!!!!! Line 306 -------------")
-						//console.log(partGeo2)
-						//console.log("----------------------------------------")
-
 
             self.reflectGeo(partGeo2, true, false);
 
-						var geometries = [self.geometry, partGeo2];
-						partGeo2 = THREE.BufferGeometryUtils.mergeGeometries(geometries);
-						//console.log("MERGING GEOMETRY LINE 309----------------")
-						//console.log(partGeo2)
+	var geometries = [self.geometry, partGeo2];
+	partGeo2 = THREE.BufferGeometryUtils.mergeGeometries(geometries);
+						
             self.geometry=partGeo2;
 
-						// partGeo = mergedGeometry;
-						// partGeo = THREE.BufferGeometry().fromGeometry(partGeo)
 
-						//console.log("DOES CONVERSION WORK??? LINE 315----------")
-						//console.log(partGeo2)
-						//console.log("-----------------------")
-
-
-            // self.geometry.merge(partGeo2);
             partGeo2.dispose();
 
             if (symmetryType > 1) {
@@ -401,17 +204,9 @@ function Building(xPos, yPos) {
                 partGeo3.copy(partGeo);
                 self.reflectGeo(partGeo3, false, true);
 
-								geometries = [self.geometry, partGeo3];
-								partGeo3 = THREE.BufferGeometryUtils.mergeGeometries(geometries);
-								//console.log("MERGING GEOMETRY LINE 339----------------")
-								//console.log(partGeo3)
-
-								// partGeo = mergedGeometry;
-								// partGeo = THREE.BufferGeometry().fromGeometry(partGeo)
-
-								//console.log("DOES CONVERSION WORK??? LINE 345----------")
-								//console.log(partGeo3)
-								//console.log("-----------------------")
+		geometries = [self.geometry, partGeo3];
+		partGeo3 = THREE.BufferGeometryUtils.mergeGeometries(geometries);
+		
 
                 self.geometry=partGeo3;
                 // self.geometry.merge(partGeo3);
@@ -482,10 +277,6 @@ function Building(xPos, yPos) {
         self.generateGeometry();
     } else {
         self.isStreet = true;
-				//console.log("LINE 238 sending self.bufferGeometry-------------")
-				//console.log(self.geometry)
-				//console.log("COLOR VALUE!!!!")
-				//console.log(new THREE.Color().setHSL(hue, 0.0, 0.1))
         self.setFaceColor(self.geometry, new THREE.Color().setHSL(hue, 0.0, 0.1));
         self.createMesh();
     }
